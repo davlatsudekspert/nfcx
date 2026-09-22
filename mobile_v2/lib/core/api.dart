d6963707e1f04504449978368ef94da7ce51540d
davlatsudekspert <davlatsudekspert@gmail.com>
2026-09-22 21:39:50 +0500

    V2: add PUT support for profile updates

--- mobile_v2/lib/core/api.dart
+++ mobile_v2/lib/core/api.dart
@@ -60,6 +60,10 @@ class ApiClient {
         () => _http.post(_uri(path), headers: _headers(json: true), body: jsonEncode(body ?? const {})),
       );
 
+  Future<dynamic> put(String path, [Object? body]) => _send(
+        () => _http.put(_uri(path), headers: _headers(json: true), body: jsonEncode(body ?? const {})),
+      );
+
   Future<AuthResponse> postAuth(String path, Object? body) async {
     final res = await _raw(
       () => _http.post(_uri(path), headers: _headers(json: true), body: jsonEncode(body ?? const {})),
